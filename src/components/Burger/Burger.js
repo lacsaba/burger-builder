389e575e3ee02b33823bd5d89454fe31bd8e55e1
Csaba Lakatos <lacsaba@gmail.com>
2018-04-14 12:53:09 +0200

    - adding state to the burger builder - handling ingredients dynamically in the burger - adding empty burger message

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,21 +4,27 @@ import PropTypes from 'prop-types';
 import Ingredient from './Ingredient/Ingredient';
 import classes from './Burger.css';
 
-const Burger = (props) => {
+const Burger = ({ingredients}) => {
+  let transformedIngredients = Object.keys(ingredients)
+    .map(key => [...Array(ingredients[key])]
+      .map((_, i) => <Ingredient key={key + i} type={key}/>))
+    .reduce((arr, el) => arr.concat(el), []);
+
+  if (!transformedIngredients.length) {
+    transformedIngredients = <p>Please start adding ingredients.</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <Ingredient type="bread-top"/>
-      <Ingredient type="salad"/>
-      <Ingredient type="cheese"/>
-      <Ingredient type="meat"/>
-      <Ingredient type="cheese"/>
+      {transformedIngredients}
       <Ingredient type="bread-bottom"/>
     </div>
   );
 };
 
 Burger.propTypes = {
-  //myProp: PropTypes.object.isRequired
+  ingredients: PropTypes.object.isRequired
 };
 
 export default Burger;
