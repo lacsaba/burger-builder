83a595f9168745f9de7af4469ff2b7d69cb1605a
lacsaba <lacsaba@gmail.com>
2018-06-22 17:38:17 +0200

    - added logout

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -18,6 +18,18 @@ export const authFail = error => ({
   error
 });
 
+export const logout = () => ({
+  type: actionTypes.AUTH_LOGOUT
+});
+
+export const checkAuthTimeout = expirationTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
+
 export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart());
@@ -38,6 +50,7 @@ export const auth = (email, password, isSignup) => {
       .then(response => {
         console.log(response);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch(err => {
         console.log(err);
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -12,7 +12,8 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START: return updateObject(state, {error: null, loading: true});
     case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
-    case actionTypes.AUTH_FAIL: return updateObject(state, {error: action.error, loading: false})
+    case actionTypes.AUTH_FAIL: return updateObject(state, {error: action.error, loading: false});
+    case actionTypes.AUTH_LOGOUT: return updateObject(state, {token: null, userId: null});
     default: return state;
   }
 };
