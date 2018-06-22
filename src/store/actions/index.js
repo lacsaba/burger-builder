c8750ffbebd4f38e20d6a093805d72d991f4df95
lacsaba <lacsaba@gmail.com>
2018-06-22 21:33:10 +0200

    - added logout link and Logout component

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,3 @@
 export {addIngredient, initIngredients, removeIngredient} from './burgerBuilder';
 export {fetchOrders, purchaseBurger, purchaseInit} from './order';
-export {auth} from './auth';
+export {auth, logout} from './auth';
