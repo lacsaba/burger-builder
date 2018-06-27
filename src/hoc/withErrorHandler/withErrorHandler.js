0ed4e8d751ef704e01ad33d6e3e68985284510bb
lacsaba <lacsaba@gmail.com>
2018-06-27 23:19:10 +0200

    - added another test

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,7 +9,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null
     };
 
-    componentWillMount() {
+    componentDidMount() {
       this.requestInterceptor = axios.interceptors.request.use(req => {
         this.setState({error: null});
 
